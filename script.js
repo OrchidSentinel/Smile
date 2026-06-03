844b76afa2a7b373b794b0fc21c621c76bf69416
Orchid <leske.annika@gmail.com>
2026-06-03 13:56:34 +0200

    Add sample function for different probe responses

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ result.innerHTML = "ACCESS DENIED // DAS LÄCHELN HAT DICH GESEHEN";
 result.style.color = "#ff0000";
 }
 }
+
+function sample(id) {
+const result = document.getElementById("sampleResult");
+
+if (id === "A") {
+result.innerHTML = "PROBE A // Menschlich // Angst hoch // Adrenalin instabil";
+}
+
+if (id === "B") {
+result.innerHTML = "PROBE B // Vampirisch // Blutaktivität normal // Durstreaktion vorhanden";
+}
+
+if (id === "C") {
+result.innerHTML = "PROBE C // Unbekannt // Kein Durst // Kein Schmerz // Kein Puls // Lächeln aktiv";
+}
+
+if (id === "D") {
+result.innerHTML = "PROBE D // Kontaminiert // Spuren von Pilzprotein // nicht stabil";
+}
+
+result.style.color = "#ff2a2a";
+}
+
+function checkArchivePassword() {
+const pass = document.getElementById("password").value.trim().toUpperCase();
+const result = document.getElementById("result");
+
+if (pass === "DURST") {
+window.location.href = "archive2.html";
+} else {
+result.innerHTML = "FALSCH // DIE PROBE REAGIERT NICHT";
+result.style.color = "#ff0000";
+}
+}
